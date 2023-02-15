88d3be3fbf69f6ebf8af6f5820b7c0c7588e7452
TianYuan <white-sky@qq.com>
2023-02-15 10:06:28 +0000

    mv utils and ipynb

--- slim_infer_unittest/gan_vocoders/gan_vocoders_unit_test.py
+++ slim_infer_unittest/gan_vocoders/gan_vocoders_unit_test.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../')
+sys.path.append('../../')
 from pathlib import Path
 
 import jsonlines
--- slim_infer_unittest/speedyspeech/speedyspeech_unit_test.py
+++ slim_infer_unittest/speedyspeech/speedyspeech_unit_test.py
@@ -1,6 +1,6 @@
 
 import sys
-sys.path.append('../')
+sys.path.append('../../')
 from pathlib import Path
 
 import jsonlines
